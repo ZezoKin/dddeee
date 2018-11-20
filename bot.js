0d9c07120d661a33f0ab02bf4b0c08fd3c171449
ZezoKin <44434287+ZezoKin@users.noreply.github.com>
2018-11-20 20:55:15 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,7 +7,7 @@ const moment = require('moment');
 var prefix = "+";
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(` اكتب اي شي هنا  `,"http://twitch.tv/S-F")
+client.user.setGame(` المملكة الزرقاء  `,"http://twitch.tv/S-F")
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
